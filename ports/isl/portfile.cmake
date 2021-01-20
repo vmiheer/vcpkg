cca43356af8d344bdceedb0ffb6b411402b92f78
Miheer Vaidya <m.vaidya@utah.edu>
2021-01-19 21:10:43 -0700

    Some updates

--- ports/isl/portfile.cmake
+++ ports/isl/portfile.cmake
@@ -32,13 +32,21 @@
 # # Specifies if the port install should fail immediately given a condition
 # vcpkg_fail_port_install(MESSAGE "isl currently only supports Linux and Mac platforms" ON_TARGET "Windows")
 
-vcpkg_from_github(
+# vcpkg_from_github(
+#     OUT_SOURCE_PATH SOURCE_PATH
+#     REPO vmiheer/isl
+#     HEAD_REF patch-1
+#     REF 0ba59d683d0abbecc54a5bec2901084be2550049
+#     SHA512 1
+# )
+
+vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO Meinersbur/isl
-    HEAD_REF cmake
-    REF 8bee467c7170c47653e04e16e49a2e190d1440f4
-    SHA512 d657e1843c620d5b7caa953d9a16cf858f0819f1cdb92ce840b77dbbfe0006c13e70c3e5418732b645262782a97bbd5ab7f87d65d3156b97794bbf2c38b22e76
+    URL C:/Users/mvaidya/source/repos/isl
+    REF 276fe83f9cb3f0485181460df80485ef588ffd39
 )
+
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -46,41 +54,17 @@ vcpkg_configure_cmake(
     OPTIONS
         ${OPTIONS}
         -DISL_INT=gmp
-        -DWERROR=OFF
+        -DISL_SMALL_INT_OPT=OFF
 )
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
-# file(INSTALL ${SOURCE_PATH}/license.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
-# # Check if one or more features are a part of a package installation.
-# # See /docs/maintainers/vcpkg_check_features.md for more details
-# vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
-#   FEATURES # <- Keyword FEATURES is required because INVERTED_FEATURES are being used
-#     tbb   WITH_TBB
-#   INVERTED_FEATURES
-#     tbb   ROCKSDB_IGNORE_PACKAGE_TBB
-# )
-
-# vcpkg_configure_make(
-#     SOURCE_PATH ${SOURCE_PATH}
-#     AUTOCONFIG
-#     OPTIONS
-#         --with-gmp-prefix='${prefix}'
-#     OPTIONS_DEBUG
-#         --enable-debug
-# )
-# vcpkg_configure_cmake(
-#     SOURCE_PATH ${SOURCE_PATH}
-#     PREFER_NINJA # Disable this option if project cannot be built with Ninja
-#     # OPTIONS -DUSE_THIS_IN_ALL_BUILDS=1 -DUSE_THIS_TOO=2
-#     # OPTIONS_RELEASE -DOPTIMIZE=1
-#     # OPTIONS_DEBUG -DDEBUGGABLE=1
-# )
 
 # vcpkg_install_cmake()
 
 # # Moves all .cmake files from /debug/share/isl/ to /share/isl/
 # # See /docs/maintainers/vcpkg_fixup_cmake_targets.md for more details
-# vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/isl)
 
+vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/isl)
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 # # Handle copyright
-# file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/isl RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/isl RENAME copyright)
